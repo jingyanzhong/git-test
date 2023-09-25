6b480625040d5f232e4107e8eda1a7590f98ea25
jingyanzhong <melody804210@gmail.com>
2023-09-25 18:05:53 +0800

    衝突修正

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of ab334a4 (刪除內容)
     <p>新增2</p>
     <ul>
       <li><a href="">1</a></li>
@@ -15,14 +11,8 @@
       <li><a href="">6</a></li>
     </ul>
     <a href="#">按鈕</a>
-<<<<<<< HEAD
     <p>123</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione optio porro repellendus eos ut odit officia necessitatibus asperiores dolor. Repellat, magni accusantium? Unde optio enim aliquam nostrum dolor iste totam?</p>
-=======
->>>>>>> git-test/master
-=======
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione optio porro repellendus eos ut odit officia necessitatibus asperiores dolor. Repellat, magni accusantium? Unde optio enim aliquam nostrum dolor iste totam?</p>
->>>>>>> parent of ab334a4 (刪除內容)
   </div>
 </template>
 
