ab334a461ef5fe55f5da1f75809bd1f14beb6f82
jingyanzhong <melody804210@gmail.com>
2023-09-25 17:22:16 +0800

    刪除內容

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>新增2</p>
-    <ul>
-      <li><a href="">1</a></li>
-      <li><a href="">2</a></li>
-      <li><a href="">3</a></li>
-      <li><a href="">4</a></li>
-      <li><a href="">5</a></li>
-      <li><a href="">6</a></li>
-    </ul>
-    <a href="#">按鈕</a>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione optio porro repellendus eos ut odit officia necessitatibus asperiores dolor. Repellat, magni accusantium? Unde optio enim aliquam nostrum dolor iste totam?</p>
   </div>
 </template>
 
