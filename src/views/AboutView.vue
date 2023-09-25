643d7d2833786e45ac837b020bf20d998bf8aed5
jingyanzhong <melody804210@gmail.com>
2023-09-25 16:45:47 +0800

    新增ul li

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-<<<<<<< HEAD
-    <p>新增2</p>
-=======
->>>>>>> parent of 2c07a1c (新增p段落)
+    <ul>
+      <li>
+        <p>1</p>
+      </li>
+      <li>
+        <p>2</p>
+      </li>
+      <li>
+        <p>3</p>
+      </li>
+      <li>
+        <p>4</p>
+      </li>
+      <li>
+        <p>5</p>
+      </li>
+    </ul>
   </div>
 </template>
 
