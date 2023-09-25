01ea7a19abf4da82bf0bc41a44f40bc7930b9b62
jingyanzhong <melody804210@gmail.com>
2023-09-25 17:16:45 +0800

    no message

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,15 +2,19 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p>新增2</p>
+    <ul>
+      <li><a href="">1</a></li>
+      <li><a href="">2</a></li>
+      <li><a href="">3</a></li>
+      <li><a href="">4</a></li>
+      <li><a href="">5</a></li>
+      <li><a href="">6</a></li>
+    </ul>
+    <a href="#">按鈕</a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione optio porro repellendus eos ut odit officia necessitatibus asperiores dolor. Repellat, magni accusantium? Unde optio enim aliquam nostrum dolor iste totam?</p>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
