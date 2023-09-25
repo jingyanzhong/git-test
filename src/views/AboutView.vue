52192464c3e4f239c849a5f20d9d4424d137cbfb
jingyanzhong <melody804210@gmail.com>
2023-09-25 17:30:19 +0800

    revert test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,9 @@
   <div class="about">
     <h1>This is an about page</h1>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of ab334a4 (刪除內容)
     <p>新增2</p>
     <ul>
       <li><a href="">1</a></li>
@@ -12,10 +15,14 @@
       <li><a href="">6</a></li>
     </ul>
     <a href="#">按鈕</a>
+<<<<<<< HEAD
     <p>123</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione optio porro repellendus eos ut odit officia necessitatibus asperiores dolor. Repellat, magni accusantium? Unde optio enim aliquam nostrum dolor iste totam?</p>
 =======
 >>>>>>> git-test/master
+=======
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione optio porro repellendus eos ut odit officia necessitatibus asperiores dolor. Repellat, magni accusantium? Unde optio enim aliquam nostrum dolor iste totam?</p>
+>>>>>>> parent of ab334a4 (刪除內容)
   </div>
 </template>
 
